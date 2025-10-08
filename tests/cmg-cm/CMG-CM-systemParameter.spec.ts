54a4dfa7a6349cc37727a30337a02d2bfed37327
subhankar1982 <subhankar.dey@mitel.com>
2025-10-08 17:44:20 +0530

    new changes

--- tests/cmg-cm/CMG-CM-systemParameter.spec.ts
+++ tests/cmg-cm/CMG-CM-systemParameter.spec.ts
@@ -4,9 +4,9 @@ import { CMGCMTestHelpers } from '../../config/cmg-cm/test-helpers';
 
 const config = getConfig(process.env.TEST_ENV);
 
-test('CMG Configuration Manager login and logout test', async ({ page }) => {
+test('CMG Configuration Manager system parameters test', async ({ page }) => {
   const helpers = new CMGCMTestHelpers(page, config);
   
-  // Perform complete login, verify, and logout flow with parameterized data
-  await helpers.completeLoginLogoutFlow();
+  // Perform complete system parameters configuration flow with parameterized data
+  await helpers.systemParametersFlow();
 });
